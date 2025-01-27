43d90dc400e238dadf0a41fd17c658ccade82fe1
shriyarani <siabansal513@gmail.com>
2025-01-27 08:25:37 +0530

    Learning merge

--- ThreadUsingLambda.java
+++ ThreadUsingLambda.java
@@ -6,7 +6,7 @@ public class ThreadUsingLambda {
             }
         };
 
-        Thread thread = new Thread(runnable);
-        thread.run();
+        Thread threads = new Thread(runnable);
+        threads.run();
     }
 }
